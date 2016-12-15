81609c054d00c1feca585a6f32a96736a2314ba3
Dan Lee <danl@admins-MacBook-Pro-3.local>
2016-12-15 10:43:41 -0800

    starting to modify config files

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+# use keychain github credentials
+git config --global credential.helper osxkeychain
 
 
 # install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# ctrl-r fuzzy finder
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# vim-plug package manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# show hidden files in finder
+defaults write com.apple.finder AppleShowAllFiles YES
 
---
+++ vimrc
@@ -0,0 +1,6 @@
+call plug#begin('~/.vim/plugged')
+
+Plug 'tpope/vim-sensible'
+Plug 'junegunn/vim-easy-align'
+
+call plug#end()
