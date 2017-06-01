e4fa8149cdd37c80089de1bf18dfd44e6cbbb165
Dan Lee <dan.lee@plangrid.com>
2017-06-01 14:49:14 -0700

    add preview to dock

--- dock.sh
+++ dock.sh
@@ -7,6 +7,7 @@ dockutil --add '/Applications/iTerm.app'
 dockutil --add '/Applications/Safari.app'
 dockutil --add '/Applications/Google Chrome.app'
 dockutil --add '/Applications/Spotify.app'
+dockutil --add '/Applications/Preview.app'
 dockutil --add '/Applications/Calendar.app'
 dockutil --add '/Applications' --view grid --display stack
 dockutil --add '~/Downloads' --view fan --display folder --allhomes
