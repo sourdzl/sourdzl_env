bb14d21d0cf5f190e117fae5766adead3753d4a6
dlee <dlee@pinterest.com>
2021-06-09 16:44:52 -0700

    add pinterest python3 venv for arc;

--- pinterest.sh
+++ pinterest.sh
@@ -7,6 +7,13 @@ git config --global user.name dlee
 cd ~/code
 git clone --depth 1 ssh://git@phabricator.pinadmin.com/diffusion/P/pinboard.git
 
+cd ~/code/pinboard
+python3 -m venv .venv         # .venv is the name of the virtual environment
+source .venv/bin/activate     # This "activates" the virtual environment. When the virtual env in active, you
+                                # should see (.venv) to the left of your terminal prompt after this step
+pip3 install -r requirements/lint.txt    
+ 
+
 # watcher
 /usr/local/bin/python3 -m pip install --upgrade pip
 python3 -m pip install macfsevents watchdog
@@ -16,7 +23,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 nvm install 14 && nvm alias default v14 && nvm use default # Install node
 npm i -g yarn
-cd ~/code/pinboard/webapp && yarn
+# cd ~/code/pinboard/webapp && yarn
 
 # arcanist
 arc set-config editor vim
